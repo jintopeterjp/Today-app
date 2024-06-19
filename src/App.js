fe413de2e73fcc449e3c9d6344328d90f9d36917
jinto peter <jinto.peter3@gmail.com>
2024-06-19 21:17:43 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import TodoApp from "./component/TodoApp";
+
+
+class App extends Component {
+
+  render(){
+    return(
+      <div>
+      <TodoApp></TodoApp>
+    
+      </div>
+    );
+  }
+ 
+}
+export default App;
